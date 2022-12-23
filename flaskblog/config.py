4a7357e2cee09370eca3a77091c72dd73bf98770
tunghng <lehoangtung081003@gmail.com>
2022-12-23 11:16:37 +0700

    first and last commit

---
+++ flaskblog/config.py
@@ -0,0 +1,10 @@
+import os 
+
+class Config:
+    SECRET_KEY = '05f0093f6b01d685882bf7abf47e886a'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
